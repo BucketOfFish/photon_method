3b07b3b64ed5e055c8164b9528025eb00f3f05ee
Matt Zhang <set.stun@gmail.com>
2019-05-21 03:43:31 +0200

    Updated data18 luminosity

--- Common/CommonFunctions.C
+++ Common/CommonFunctions.C
@@ -20,7 +20,7 @@ void CopyBranch(TTree* inputTree, TTree* outputTree, string inputBranchName, str
 //  period: data15-16 (input) -> ZMC16a (source file), data17 -> ZMC16cd, data18 -> ZMC16e
 float GetLumi(TString period) {
     float lumi = 1.0;
-    if (period.Contains("mc16e") || period.Contains("data18")) lumi = 60000; // "mc16cd_2018"? <UPDATE>
+    if (period.Contains("mc16e") || period.Contains("data18")) lumi = 59900;
     else if (period.Contains("mc16cd") || period.Contains("data17")) lumi = 44000;
     else if (period.Contains("mc16a") || period.Contains("data15-16")) lumi = 36100;
     return lumi;
