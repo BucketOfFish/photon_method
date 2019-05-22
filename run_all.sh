247d14b001b45b408204f7004740d8b9b94414d7
Matt Zhang <set.stun@gmail.com>
2019-05-22 05:23:21 +0200

    Changed run script to use new ntuple maker code syntax

--- run_all.sh
+++ run_all.sh
@@ -6,15 +6,6 @@ lsetup "root 6.14.04-x86_64-slc6-gcc62-opt"
 
 cd NtupleMaker/
 
-#root -l -b -q 'GetPhotonEvents.C("SinglePhoton222_merged_processed","gmc","/eos/atlas/user/y/ycao/SUSY_dataset/JETM4_v1.6/JETM4_mc16a/","MC","SinglePhoton222_NoSys")'
-
-#root -l -b -q 'GetBaseLineEvents.C("Zjets_merged_processed","ZMC16a","/eos/atlas/atlascerngroupdisk/phys-susy/2L2J-ANA-SUSY-2018-05/SusySkim2LJets/v1.6/SUSY2/SUSY2_Bkgs_mc16a/","MC","Zjets_NoSys")'
-#root -l -b -q 'GetBaseLineEvents.C("ttbar_merged_processed","ZMC16a","/eos/atlas/atlascerngroupdisk/phys-susy/2L2J-ANA-SUSY-2018-05/SusySkim2LJets/v1.6/SUSY2/SUSY2_Bkgs_mc16a/","MC","ttbar_NoSys")'
-#root -l -b -q 'GetBaseLineEvents.C("diboson_merged_processed","ZMC16a","/eos/atlas/atlascerngroupdisk/phys-susy/2L2J-ANA-SUSY-2018-05/SusySkim2LJets/v1.6/SUSY2/SUSY2_Bkgs_mc16a/","MC","diboson_NoSys")'
-
-#root -l -b -q 'GetPhotonEvents.C("data15-16_merged_processed","gdata","/eos/atlas/user/y/ycao/SUSY_dataset/JETM4_v1.6/JETM4_Data/","Data","data15-16")'
-#root -l -b -q 'GetBaseLineEvents.C("data15-16_merged_processed","zdata","/eos/atlas/atlascerngroupdisk/phys-susy/2L2J-ANA-SUSY-2018-05/SusySkim2LJets/v1.6/SUSY2/SUSY2_Data/","Data","data")'
-
 root -l -b -q 'MakeNtuple.C("SinglePhoton222_merged_processed","gmc","/eos/atlas/user/y/ycao/SUSY_dataset/JETM4_v1.6/JETM4_mc16a/","MC","SinglePhoton222_NoSys","photon")'
 
 root -l -b -q 'MakeNtuple.C("Zjets_merged_processed","ZMC16a","/eos/atlas/atlascerngroupdisk/phys-susy/2L2J-ANA-SUSY-2018-05/SusySkim2LJets/v1.6/SUSY2/SUSY2_Bkgs_mc16a/","MC","Zjets_NoSys","non-photon")'
