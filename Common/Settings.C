71e55e3b354f2189cc842589880d6fbaa753b850
Matt Zhang <set.stun@gmail.com>
2019-05-24 19:24:31 +0200

    Another bug fix for Settings

--- Common/Settings.C
+++ Common/Settings.C
@@ -1,7 +1,7 @@
 #ifndef COMMON_SETTINGS
 #define COMMON_SETTINGS
 
-sample_folder = "NonLepIso"
+std::string sample_folder = "NonLepIso";
 std::string ntuple_path = "/eos/user/m/mazhang/PhotonMethod/v1.6/" + sample_folder + "/Ntuples/";
 std::string smearing_path = "/eos/user/m/mazhang/PhotonMethod/v1.6/" + sample_folder + "/SmearedNtuples/";
 std::string reweighting_path = "/eos/user/m/mazhang/PhotonMethod/v1.6/" + sample_folder + "/ReweightedNtuples/";
