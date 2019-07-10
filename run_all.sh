6805d289784b5c07c40abf4016002256b4c807a1
Matt Zhang <set.stun@gmail.com>
2019-07-10 13:51:44 +0200

    Added pointer to newly produced photon samples with low pT threshold

--- run_all.sh
+++ run_all.sh
@@ -6,7 +6,8 @@ lsetup "root 6.14.04-x86_64-slc6-gcc62-opt"
 
 cd NtupleMaker/
 
-export PHOTON_MC_PATH='/eos/atlas/user/y/ycao/SUSY_dataset/JETM4_v1.6/JETM4_'
+#export PHOTON_MC_PATH='/eos/atlas/user/y/ycao/SUSY_dataset/JETM4_v1.6/JETM4_'
+export PHOTON_MC_PATH='/eos/user/m/mazhang/PhotonMethod/v1.6/xAOD/Merged/JETM4_'
 export PHOTON_DATA_PATH='/eos/atlas/user/y/ycao/SUSY_dataset/JETM4_v1.6/JETM4_Data/'
 export BKG_MC_PATH='/eos/atlas/atlascerngroupdisk/phys-susy/2L2J-ANA-SUSY-2018-05/SusySkim2LJets/v1.6/SUSY2/SUSY2_Bkgs_'
 export BKG_DATA_PATH='/eos/atlas/atlascerngroupdisk/phys-susy/2L2J-ANA-SUSY-2018-05/SusySkim2LJets/v1.6/SUSY2/SUSY2_Data/'
