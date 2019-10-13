c69aadcd893e5a4bb701d5e0a8a042d5905fde4d
Matt Zhang <set.stun@gmail.com>
2019-10-14 01:08:15 +0200

    Fixed a bug with vector branches in MakeNtuple.C

--- Common/CommonFunctions.C
+++ Common/CommonFunctions.C
@@ -19,7 +19,7 @@ void CopyBranch(TTree* inputTree, TTree* outputTree, string inputBranchName, str
         if (branch->GetName() == inputBranchName) {
             inputTree->SetBranchStatus(inputBranchName.c_str(), 1);
             inputTree->SetBranchAddress(inputBranchName.c_str(), variablePointer);
-            if (varType.compare(0, 11, "std::vector") == 0)
+            if (varType.compare(0, 6, "vector") == 0)
                 outputTree->Branch(outputBranchName.c_str(), varType.c_str(), variablePointer);
             else
                 outputTree->Branch(outputBranchName.c_str(), variablePointer, (outputBranchName+"/"+varType).c_str());
