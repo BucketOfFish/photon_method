107755399c6c8440a17e884549542a4ffc2d9023
Matt Zhang <set.stun@gmail.com>
2019-10-23 22:39:18 +0200

    Pushed CR up to MET<100 from MET<60

--- Common/Settings.C
+++ Common/Settings.C
@@ -1,12 +1,12 @@
 #ifndef COMMON_SETTINGS
 #define COMMON_SETTINGS
 
-std::string sample_folder = "/eos/user/m/mazhang/PhotonMethod/v1.7/Default/";
-std::string sampling_method = "HistogramSampling";
+std::string sample_folder = "/eos/user/m/mazhang/PhotonMethod/v1.7/TinySamples_NoPileupWeight/";
+std::string sampling_method = "NaiveSampling";
 
 std::string ntuple_path =  sample_folder + "Ntuples/";
 std::string smearing_path = sample_folder + "/" + sampling_method + "/SmearedNtuples/";
-std::string reweighting_path = sample_folder + "/" + sampling_method + "/ReweightedNtuples4/";
+std::string reweighting_path = sample_folder + "/" + sampling_method + "/ReweightedNtuples/";
 std::string plots_path = sample_folder + "/" + sampling_method + "/Plots/";
 
 namespace cuts {
@@ -22,7 +22,7 @@ namespace cuts {
     //TCut reweight_region("nJet30>=2 && met_Et<200");
     TCut reweight_region("nJet30>=2 && lepPt[0]>25.0 && lepPt[1]>25.0");
 
-    TCut CR("met_Et<60.0");
+    TCut CR("met_Et<100.0");
     TCut VR("nJet30>=2 && lepCharge[0]*lepCharge[1]<0 && abs(lepFlavor[0])==abs(lepFlavor[1]) && lepPt[0]>25 && lepPt[1]>25 && jet_pT[0]>30 && jet_pT[1]>30 && mll>80 && mll<100 && mjj<60 && mjj>100");  
     TCut SR("nJet30>=2 && lepPt[0]>25 && lepPt[1]>25 && jet_pT[0]>30 && jet_pT[1]>30 && nBJet20_MV2c10_FixedCutBEff_77==0 && (mjj<60.0 || mjj>100.)");
 
