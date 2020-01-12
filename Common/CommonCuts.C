7656e2990d999f7f15728084d150d1966afe4a4e
Matt Zhang <set.stun@gmail.com>
2020-01-12 18:36:36 +0100

    Updates to cuts and bins

--- Common/CommonCuts.C
+++ Common/CommonCuts.C
@@ -7,15 +7,11 @@ namespace cuts {
     TCut photon_baseline("nJet30>=2 && lepPt[0]>25.0 && lepPt[1]>25.0");
     TCut baseline("nJet30>=2 && lepPt[0]>25.0 && lepPt[1]>25.0");
 
-    //TCut reweight_region("nJet30>=2 && met_Et>100 && met_Et<200");
-    //TCut reweight_region("nJet30>=2 && met_Et<200");
     TCut reweight_region("nJet30>=2 && lepPt[0]>25.0 && lepPt[1]>25.0");
 
-    //TCut CR("met_Et<100.0 && Ptll>25");
+    TCut SR("nJet30>=2 && lepPt[0]>25.0 && lepPt[1]>25.0");
     TCut CR("met_Et<100.0");
-    //TCut VR("nJet30>=2 && lepCharge[0]*lepCharge[1]<0 && abs(lepFlavor[0])==abs(lepFlavor[1]) && lepPt[0]>25 && lepPt[1]>25 && jet_pT[0]>30 && jet_pT[1]>30 && mll>80 && mll<100 && mjj<60 && mjj>100");  
     TCut VR("nJet30>=2 && lepPt[0]>25 && lepPt[1]>25 && jet_pT[0]>30 && jet_pT[1]>30 && mll>80 && mll<100 && mjj<60 && mjj>100");  
-    TCut SR("nJet30>=2 && lepPt[0]>25 && lepPt[1]>25 && jet_pT[0]>30 && jet_pT[1]>30 && nBJet20_MV2c10_FixedCutBEff_77==0 && (mjj<60.0 || mjj>100.)");
 
     TCut VRcom("nJet30>=2 && lepCharge[0]*lepCharge[1]<0 && abs(lepFlavor[0])==abs(lepFlavor[1]) && lepPt[0]>25 && lepPt[1]>25 && jet_pT[0]>30 && jet_pT[1]>30 && met_Et>100 && met_Et<200");  
     // from https://arxiv.org/pdf/1611.05791.pdf
