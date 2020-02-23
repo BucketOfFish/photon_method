2d5fdcc331a46f98a76a592bdc7ab735ab6da609
Matt Zhang <set.stun@gmail.com>
2020-02-23 19:51:49 +0100

    Fixed bug where no photon events were being saved

--- Studies/StoreMETlData.py
+++ Studies/StoreMETlData.py
@@ -1,13 +1,16 @@
+from __future__ import print_function
 import ROOT
 import pickle as pkl
 import numpy as np
+import sys
 
 
 sample_folder = "/eos/user/m/mazhang/PhotonMethod/v1.7/Samples/"
 ntuple_path = sample_folder + "Ntuples/"
 mc_period = "mc16e"
 pt_bins = [40, 45, 50, 55, 60, 70, 80, 100, 120, 140, 160, 180, 200, 220, 260, 280, 300, 350, 400, 600, 1000]
-max_n_events = 10000000
+# max_n_events = 10000000
+max_n_events = -1
 
 # ----------
 # Get Z METl
@@ -16,13 +19,18 @@ max_n_events = 10000000
 Z_mc_file = ROOT.TFile(ntuple_path + "/bkg_mc/" + mc_period + "_Zjets.root")
 Z_mc_tree = Z_mc_file.BaselineTree
 
-Z_METl = [[] for _ in range(len(pt_bins)-1)]
+print("Processing " + str(Z_mc_tree.GetEntries()) + " Z events")
+Z_METl = [[] for _ in range(len(pt_bins))]
 for i, event in enumerate(Z_mc_tree):
-    if i > max_n_events:
+    if max_n_events > -1 and i > max_n_events:
         break
+    if i % 100000 == 0:
+        print(str(i) + " events processed", end="\r")
+        sys.stdout.flush()
     if event.Ptll > 40 and event.nJet30 >= 2 and event.jet_pT[0] > 30 and event.jet_pT[1] > 30 and event.nLep_signal >= 2 and event.lepPt[0] > 25 and event.lepPt[1] > 25:
         bin_n = np.digitize([event.Ptll], pt_bins)[0] - 1
         Z_METl[bin_n].append(event.METl)
+print()
 
 Z_mc_file.Close()
 
@@ -33,13 +41,18 @@ Z_mc_file.Close()
 photon_mc_file = ROOT.TFile(ntuple_path + "/g_mc/" + mc_period + "_SinglePhoton222.root")
 photon_mc_tree = photon_mc_file.BaselineTree
 
+print("Processing " + str(photon_mc_tree.GetEntries()) + " photon events")
 photon_METl = [[] for _ in range(len(pt_bins)-1)]
 for i, event in enumerate(photon_mc_tree):
-    if i > max_n_events:
+    if max_n_events > -1 and i > max_n_events:
         break
+    if i % 100000 == 0:
+        print(str(i) + " events processed", end="\r")
+        sys.stdout.flush()
     if event.Ptll > 40 and event.nJet30 >= 2 and event.jet_pT[0] > 30 and event.jet_pT[1] > 30:
         bin_n = np.digitize([event.Ptll], pt_bins)[0]-1
         photon_METl[bin_n].append(event.METl)
+print()
 
 photon_mc_file.Close()
 
