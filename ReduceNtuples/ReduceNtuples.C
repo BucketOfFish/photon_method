d2f66c511f04b7f0730d7f2c138f61f8df6c149e
Matt Zhang <set.stun@gmail.com>
2020-03-11 15:07:48 -0500

    ReduceNtuples now renames branches correctly. Adding branches moved to normal ntuple making code

--- ReduceNtuples/ReduceNtuples.C
+++ ReduceNtuples/ReduceNtuples.C
@@ -1,6 +1,8 @@
 #include "../Common/Settings.C"
+#include <any>
 
 using namespace std;
+using BranchRenameOptions = vector<tuple<string, string>>;
 
 class TreeReducer {
 public:
@@ -10,8 +12,8 @@ public:
     string out_tree_name;
     string cut;
     vector<string> branches_to_copy;
-    vector<pair<string, string>> branches_to_rename;
-    vector<string> new_branches;
+    BranchRenameOptions branches_to_rename;
+    unordered_map<string, std::any> new_branch_vars; 
 
     TreeReducer() {
     }
@@ -23,7 +25,7 @@ public:
         this->in_file = TFile::Open(file_name.c_str());
         this->in_tree = (TTree*)this->in_file->Get(tree_name.c_str());
 
-        cout << "Events in tree         : " << in_tree->GetEntries() << endl;
+        cout << "Events in tree         : " << this->in_tree->GetEntries() << endl;
         cout << endl;
     }
 
@@ -42,27 +44,30 @@ public:
         this->branches_to_copy = branches_to_copy;
     }
 
-    void setBranchesToRename(vector<pair<string, string>> branches_to_rename) {
+    void setBranchesToRename(BranchRenameOptions branches_to_rename) {
         this->branches_to_rename = branches_to_rename;
     }
 
-    void setNewBranches(vector<string> new_branches) {
-        this->new_branches = new_branches;
-    }
-
-    void setAllBranches() {
-        this->in_tree->SetBranchStatus("*", 0);
-
+    void copyBranches() {
         for (auto branch : this->branches_to_copy) {
             this->in_tree->SetBranchStatus(branch.c_str(), 1);
         }
+    }
 
-        //for (auto branch : this->branches_to_rename) {
-        //}
-
-        for (auto branch : this->new_branches) {
+    void renameBranches() {
+        for (auto branch : this->branches_to_rename) {
+            string old_name = get<0>(branch);
+            string new_name = get<1>(branch);
+            this->in_tree->SetBranchStatus(old_name.c_str(), 1);
+            this->in_tree->GetBranch(old_name.c_str())->SetTitle(new_name.c_str());
+            this->in_tree->GetBranch(old_name.c_str())->SetName(new_name.c_str());
         }
+    }
 
+    void initAllBranches() {
+        this->in_tree->SetBranchStatus("*", 0);
+        this->copyBranches();
+        this->renameBranches();
         this->out_tree = this->in_tree->CloneTree(0);
     }
 
@@ -73,7 +78,7 @@ public:
     void write() {
         cout << "Processing" << endl;
 
-        this->setAllBranches();
+        this->initAllBranches();
 
         this->in_tree->Draw(">>event_list", this->cut.c_str(), "goff");
         TEventList *event_list = (TEventList*)gDirectory->Get("event_list");
@@ -82,6 +87,8 @@ public:
 
         for (Long64_t i=0; i<event_list->GetN(); i++) {
             this->in_tree->GetEntry(event_list->GetEntry(i));
+            for (auto var : this->new_branch_vars)
+                var.second = -999;
             this->out_tree->Fill();
         }
         this->out_tree->Write();
@@ -106,8 +113,7 @@ struct Options {
     string out_tree_name;
 
     vector<string> branches_to_copy;
-    vector<pair<string, string>> branches_to_rename;
-    vector<string> new_branches;
+    BranchRenameOptions branches_to_rename;
 
     string cut;
 
@@ -131,7 +137,6 @@ void makeReduceNtuples(Options options) {
 
     reducer->setBranchesToCopy(options.branches_to_copy);
     reducer->setBranchesToRename(options.branches_to_rename);
-    reducer->setNewBranches(options.new_branches);
 
     reducer->setCut(options.cut);
 
@@ -155,16 +160,18 @@ void throwError(string error) {
 Options setUnitTestOptions(Options options) {
     options.in_file_name = "/public/data/Photon/UnitTest/data15-16_bkg.root";
     options.out_file_name = "unit_test.root";
+    options.in_tree_name = "BaselineTree";
+    options.out_tree_name = "BaselineTree";
+
     options.branches_to_copy = vector<string> {
         "channel",
         "met_Et",
+        "lep_phi",
     };
-    options.branches_to_rename = vector<pair<string, string>> {
-        make_pair("lepPt", "lep_pT"),
+    options.branches_to_rename = BranchRenameOptions {
+        make_tuple("lepPt", "lep_pT"),
     };
-    options.new_branches = vector<string> {
-        "empty_flag",
-    };
+
     options.cut = "met_Et>300";
 
     return options;
@@ -177,14 +184,30 @@ void performUnitTests(TTree* out_tree) {
         throwError("Wrong number of events after cut");
 
     if (out_tree->GetMinimum("met_Et") > 300)
-        cout << "Skimming performed correcly" << endl;
+        cout << "Skimming performed correctly" << endl;
     else
         throwError("Skimming performed incorrectly");
 
-    if (out_tree->GetNbranches() == 2)
-        cout << "Slimming performed correcly" << endl;
+    if (out_tree->GetNbranches() == 3+1)
+        cout << "Slimming performed correctly" << endl;
     else
         throwError("Slimming performed incorrectly");
+
+    bool rename_pass = false;
+    for (auto branch : *out_tree->GetListOfBranches()) {
+        string branch_name = branch->GetName();
+        if (branch_name == "lepPt") {
+            rename_pass = false;
+            break;
+        }
+        else if (branch_name == "lep_pT") {
+            rename_pass = true;
+        }
+    }
+    if (rename_pass)
+        cout << "Branch renaming performed correctly" << endl;
+    else
+        throwError("Branch renaming performed incorrectly");
 
     cout << "Passed all unit tests" << endl;
     cout << endl;
@@ -205,12 +228,10 @@ void ReduceNtuples() {
     options.branches_to_copy = vector<string> {
         "channel",
         "met_Et",
+        "lep_phi",
     };
-    options.branches_to_rename = vector<pair<string, string>> {
-        make_pair("lepPt", "lep_pT"),
-    };
-    options.new_branches = vector<string> {
-        "empty_flag",
+    options.branches_to_rename = BranchRenameOptions {
+        make_tuple("lepPt", "lep_pT"),
     };
 
     options.cut = "met_Et>300";
