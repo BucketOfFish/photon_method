c1712d0cdfe4dfe4e78d99e419f3a45276521309
Matt Zhang <set.stun@gmail.com>
2020-03-12 03:26:42 -0500

    Updated ROOT environment on lxplus

--- setup_env.sh
+++ setup_env.sh
@@ -1,4 +1,5 @@
-lsetup "root 6.14.04-x86_64-slc6-gcc62-opt"
+#lsetup "root 6.14.04-x86_64-slc6-gcc62-opt"
+. /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.20.00/x86_64-centos7-gcc48-opt/bin/thisroot.sh
 
 export PHOTON_MC_PATH='/eos/atlas/atlascerngroupdisk/phys-susy/2L2J-ANA-SUSY-2018-05/SusySkim2LJets/v1.7/JETM4/JETM4_'
 export PHOTON_DATA_PATH='/eos/atlas/atlascerngroupdisk/phys-susy/2L2J-ANA-SUSY-2018-05/SusySkim2LJets/v1.7/JETM4/JETM4_Data/'
