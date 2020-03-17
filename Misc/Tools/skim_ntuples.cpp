2368023f41a60377860a8d169722625f50b73b44
Matt Zhang <set.stun@gmail.com>
2020-03-17 16:46:20 -0500

    Added script to skim ntuples

---
+++ Misc/Tools/skim_ntuples.cpp
@@ -0,0 +1,45 @@
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+#include <string>
+
+string oldpath = "/public/data/Photon/NewSamples/ReducedNtuples/";
+string newpath = "/public/data/Photon/UnitTest/Smearing/";
+
+vector<string> filenames{
+    "data15-16_data_bkg.root",
+    "data15-16_data_photon.root",
+    "mc16a_diboson.root",
+    "mc16a_higgs.root",
+    "mc16a_lowMassDY.root",
+    "mc16a_SinglePhoton222.root",
+    "mc16a_singleTop.root",
+    "mc16a_topOther.root",
+    "mc16a_triboson.root",
+    "mc16a_ttbar.root",
+    "mc16a_Wjets.root",
+    "mc16a_Zjets.root",
+};
+
+void skim() {
+    for (string filename : filenames) {
+        TFile *oldfile = TFile::Open((oldpath+filename).c_str());
+        TTree *oldtree = (TTree*)oldfile->Get("BaselineTree");    
+
+        TFile *newfile = new TFile((newpath+filename).c_str(), "recreate");  
+        TTree *newtree = oldtree->CloneTree(0); 
+
+        //newtree->SetMakeClass(1);
+        //oldtree->CopyAddresses(newtree);
+
+        for (Long64_t iEntry=0; iEntry<20000; iEntry++)
+        { // Main loop
+            oldtree->GetEntry(iEntry); // get ith entry
+            newtree->Fill();
+        }
+
+        newfile->Write();
+        newfile->Close();
+        delete oldfile, oldtree, newfile, newtree;
+    }
+}
