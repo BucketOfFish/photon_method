de91db238e522680a546a66bbe42862757c4cb85
Matt Zhang <set.stun@gmail.com>
2020-06-29 22:24:14 -0500

    Corrected definition for some regions, and made 2D plots for data not include MC weight

--- Misc/SUSY/2D_Plots/make_2D_eta_phi_plot.cpp
+++ Misc/SUSY/2D_Plots/make_2D_eta_phi_plot.cpp
@@ -1,14 +1,8 @@
-#include "../../PhotonMethod/Main/Settings.cpp"
+#include "../../../Main/Settings.cpp"
 
-void make_2D_eta_phi_plot() {
+void make_plots(string process, vector<string> trees) {
     // options
     string tree_name = "BaselineTree";
-    string path = "/public/data/Photon/Samples/ReducedNtuples/";
-    vector<string> trees = {
-        path + "mc16a_Zjets.root",
-        path + "mc16cd_Zjets.root",
-        path + "mc16e_Zjets.root",
-    };
     vector<vector<string>> vars = {
         {"lepEta[0]", "lepPhi[0]", "Leading Lepton Eta vs. Phi", "lep_eta_phi"},
         {"jet_eta[0]", "jet_phi[0]", "Leading Jet Eta vs. Phi", "jet_eta_phi"},
@@ -16,6 +10,7 @@ void make_2D_eta_phi_plot() {
     string weight = "totalWeight";
     vector<string> regions = {"SRC", "SRLow", "SRMed", "SRHigh", "SRLowZ", "SRMedZ", "SRHighZ", "VRC_CR", "VRC",
         "VRLow", "VRMed", "VRHigh", "VRLowZ", "VRMedZ", "VRHighZ",
+        "CRC", "CRLow", "CRMed", "CRHigh", "CRLowZ", "CRMedZ", "CRHighZ",
         "SRHigh4", "SRLow2", "VRHigh4", "EWK_VRHigh", "VRHighR", "VRllbb", "VRInt",
         "EWK_VRLow", "VRLow2", "VROffShell", "CRZZ", "CRtt", "CRZ", "CRDY", "DRHigh", "DRllbb", "DRInt", "DRLow",
         "DROffShell", "SRHigh8", "SRHigh16", "SRInt", "SRLow", "SROffShell"};
@@ -30,12 +25,37 @@ void make_2D_eta_phi_plot() {
     gStyle->SetOptStat(0);
     for (auto region : regions) {
         for (auto var : vars) {
-            TH2D *hist = new TH2D("hist", (var[2] + " for Zjets MC").c_str(), 20, -3.14159, 3.14159, 20, -2.74, 2.74);
-            chain->Draw((var[0]+":"+var[1]+">>hist").c_str(), cuts::selections[region] * TCut(weight.c_str()), "colz");
+            string title = var[2] + " for " + process + " MC";
+            if (process == "data") title = var[2] + " for " + process;
+            TH2D *hist = new TH2D("hist", title.c_str(), 20, -3.14159, 3.14159, 20, -2.74, 2.74);
+            if (process != "data") chain->Draw((var[0]+":"+var[1]+">>hist").c_str(), cuts::selections[region] * TCut(weight.c_str()), "colz");
+            else chain->Draw((var[0]+":"+var[1]+">>hist").c_str(), cuts::selections[region], "colz");
             hist->GetXaxis()->SetTitle(var[1].c_str());
             hist->GetYaxis()->SetTitle(var[0].c_str());
-            can->Print((var[3] + "_" + region + ".png").c_str());
+            can->Print((var[3] + "_" + process + "_" + region + ".png").c_str());
             delete hist;
         }
     }
+}
+
+void make_2D_eta_phi_plot() {
+    string mc_path = "/public/data/Photon/Samples/ReducedNtuples/";
+    vector<string> processes = {"diboson", "higgs", "lowMassDY", "singleTop", "topOther", "triboson", "ttbar",
+        "Vgamma", "Wjets", /*"Zjets"*/};
+    for (auto process : processes) {
+        vector<string> trees = {
+            mc_path + "mc16a_" + process + ".root",
+            mc_path + "mc16cd_" + process + ".root",
+            mc_path + "mc16e_" + process + ".root",
+        };
+        make_plots(process, trees);
+    }
+
+    string data_path = "/public/data/Photon/Samples/ReducedNtuples/";
+    vector<string> trees = {
+        data_path + "data15-16_data_bkg.root",
+        data_path + "data17_data_bkg.root",
+        data_path + "data18_data_bkg.root",
+    };
+    make_plots("data", trees);
 }
