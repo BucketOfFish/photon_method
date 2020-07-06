dd232ae35e5154f7f3e27d83609782a99f838fc3
Matt Zhang <set.stun@gmail.com>
2020-07-06 14:35:34 -0500

    Added ability to make inclusive MC plot

--- Misc/SUSY/2D_Plots/make_2D_eta_phi_plot.cpp
+++ Misc/SUSY/2D_Plots/make_2D_eta_phi_plot.cpp
@@ -8,6 +8,7 @@ void make_plots(string process, vector<string> trees, vector<string> regions) {
         {"jet_eta[0]", "jet_phi[0]", "Leading Jet Eta vs. Phi", "jet_eta_phi"},
     };
     string weight = "totalWeight";
+    string save_folder = "InclusiveMC/";
 
     // plotting
     TChain *chain = new TChain(tree_name.c_str());
@@ -19,14 +20,14 @@ void make_plots(string process, vector<string> trees, vector<string> regions) {
     gStyle->SetOptStat(0);
     for (auto region : regions) {
         for (auto var : vars) {
-            string title = var[2] + " for " + process + " MC in " + region;
+            string title = var[2] + " for " + process + " in " + region;
             if (process == "data") title = var[2] + " for data in " + region;
             TH2D *hist = new TH2D("hist", title.c_str(), 20, -3.14159, 3.14159, 20, -2.74, 2.74);
             if (process != "data") chain->Draw((var[0]+":"+var[1]+">>hist").c_str(), cuts::selections[region] * TCut(weight.c_str()), "colz");
             else chain->Draw((var[0]+":"+var[1]+">>hist").c_str(), cuts::selections[region], "colz");
             hist->GetXaxis()->SetTitle(var[1].c_str());
             hist->GetYaxis()->SetTitle(var[0].c_str());
-            can->Print((var[3] + "_" + process + "_" + region + ".png").c_str());
+            can->Print((save_folder + var[3] + "_" + process + "_" + region + ".png").c_str());
             delete hist;
         }
     }
@@ -34,20 +35,38 @@ void make_plots(string process, vector<string> trees, vector<string> regions) {
 
 void make_2D_eta_phi_plot() {
     string mc_path = "/public/data/Photon/Samples/ReducedNtuples/";
-    vector<string> processes = {"diboson", "higgs", "lowMassDY", "singleTop", "topOther", "triboson", "ttbar",
+    vector<string> all_mc_processes = {"diboson", "higgs", "lowMassDY", "singleTop", "topOther", "triboson", "ttbar",
         "Vgamma", "Wjets", "Zjets"};
-    vector<string> regions = {"SRC", "SRLow", "SRMed", "SRHigh", "SRLowZ", "SRMedZ", "SRHighZ", "VRC_CR", "VRC",
-        "VRLow", "VRMed", "VRHigh", "VRLowZ", "VRMedZ", "VRHighZ",
-        "CRC", "CRLow", "CRMed", "CRHigh", "CRLowZ", "CRMedZ", "CRHighZ",
-        "SRHigh4", "SRLow2", "VRHigh4", "EWK_VRHigh", "VRHighR", "VRllbb", "VRInt",
-        "EWK_VRLow", "VRLow2", "VROffShell", "CRZZ", "CRtt", "CRZ", "CRDY", "DRHigh", "DRllbb", "DRInt", "DRLow",
-        "DROffShell", "SRHigh8", "SRHigh16", "SRInt", "SRLow", "SROffShell"};
+    //vector<string> processes = all_mc_processes
+    vector<string> processes = {"all MC"};
+    //vector<string> regions = {"SRC", "SRLow", "SRMed", "SRHigh", "SRLowZ", "SRMedZ", "SRHighZ", "VRC_CR", "VRC",
+        //"VRLow", "VRMed", "VRHigh", "VRLowZ", "VRMedZ", "VRHighZ",
+        //"CRC", "CRLow", "CRMed", "CRHigh", "CRLowZ", "CRMedZ", "CRHighZ",
+        //"SRHigh4", "SRLow2", "VRHigh4", "EWK_VRHigh", "VRHighR", "VRllbb", "VRInt",
+        //"EWK_VRLow", "VRLow2", "VROffShell", "CRZZ", "CRtt", "CRZ", "CRDY", "DRHigh", "DRllbb", "DRInt", "DRLow",
+        //"DROffShell", "SRHigh8", "SRHigh16", "SRInt", "SRLow", "SROffShell"};
+    vector<string> regions = {"SRC", "SRLow", "SRMed", "SRHigh",
+        "VRC", "VRLow", "VRMed", "VRHigh",
+        "CRC", "CRLow", "CRMed", "CRHigh"};
     for (auto process : processes) {
-        vector<string> trees = {
-            mc_path + "mc16a_" + process + ".root",
-            mc_path + "mc16cd_" + process + ".root",
-            mc_path + "mc16e_" + process + ".root",
-        };
+        vector<string> trees = {};
+        if (process == "all MC") {
+            for (auto process : all_mc_processes) {
+                vector<string> new_trees = {
+                    mc_path + "mc16a_" + process + ".root",
+                    mc_path + "mc16cd_" + process + ".root",
+                    mc_path + "mc16e_" + process + ".root",
+                };
+                trees.insert(trees.end(), new_trees.begin(), new_trees.end());
+            }
+        }
+        else {
+            trees = {
+                mc_path + "mc16a_" + process + ".root",
+                mc_path + "mc16cd_" + process + ".root",
+                mc_path + "mc16e_" + process + ".root",
+            };
+        }
         make_plots(process, trees, regions);
     }
 
