9c1f992364ab5dd1caeb24aa5c526851294d1afe
Matt Zhang <set.stun@gmail.com>
2020-07-07 22:41:36 -0500

    Added additional regions and samples to skimming script

--- Misc/Tools/skim_ntuples_with_selection.cpp
+++ Misc/Tools/skim_ntuples_with_selection.cpp
@@ -4,12 +4,13 @@
 #include <string>
 #include "../../Main/Settings.cpp"
 
-string oldpath = "/public/data/Photon/Samples/ReweightedNtuples/";
-string newpath = "/public/data/Photon/Samples/SkimmedNtuples/VRDPhiSamples/";
-string treename = "BaselineTree";
+string oldpath = "/public/data/SUSY_Systematics/Unskimmed/Diboson/";
+string newpath = "/public/data/SUSY_Systematics/Skimmed/StrongPreselectionInclusive/";
+string treename = "diboson_NoSys";
 
 //string selection = cuts::selections["bkg_baseline"].GetTitle();
-string selection = cuts::selections["VRDPhi"].GetTitle();
+//string selection = cuts::selections["strong_preselection"].GetTitle();
+string selection = cuts::strong_preselection_noDPhi.GetTitle();
 
 map<string, vector<string>> filename_sets {
     {"reweighted",
@@ -38,8 +39,30 @@ map<string, vector<string>> filename_sets {
         {
         "mc16a_diboson.root", "mc16cd_diboson.root", "mc16e_diboson.root",
         }},
+    {"Zjets",
+        {
+        "mc16a_Zjets_merged_processed.root", "mc16cd_Zjets_merged_processed.root", "mc16e_Zjets_merged_processed.root",
+        }},
+    {"diboson",
+        {
+        "diboson_merged_processed.root",
+        }},
+    {"ttbar",
+        {
+        "ttbarDilep_410472_merged_processed.root", "ttbar_merged_processed.root",
+        }},
 };
-vector<string> filenames = filename_sets["reweighted_noMC"];
+vector<string> filenames = filename_sets["Zjets"];
+
+vector<string> branches_to_copy = vector<string> {
+    "Ht30", "LHE3Weight_MUR0.5_MUF0.5_PDF261000", "LHE3Weight_MUR0.5_MUF1_PDF261000",
+    "LHE3Weight_MUR1_MUF0.5_PDF261000", "LHE3Weight_MUR1_MUF1_PDF13000", "LHE3Weight_MUR1_MUF1_PDF25300",
+    "LHE3Weight_MUR1_MUF1_PDF261000", "LHE3Weight_MUR1_MUF2_PDF261000", "LHE3Weight_MUR2_MUF1_PDF261000",
+    "LHE3Weight_MUR2_MUF2_PDF261000", "Ptll", "RandomRunNumber", "bTagWeight", "eventWeight", "genWeight",
+    "globalDiLepTrigSF", "jvtWeight", "lepCharge", "lepFlavor", "lepPt", "leptonWeight", "met_Et", "met_Sign",
+    "minDPhi2JetsMet", "mll", "mt2leplsp_0", "nJet30", "nLep_base", "nLep_signal", "pileupWeight",
+    "trigMatch_2LTrigOR", "nBJet20_MV2c10_FixedCutBEff_77", "mjj", "jetPt", "Rll", "dPhiMetJet1", "dPhiPllMet",
+};
 
 void skim_ntuples_with_selection() {
     TreeCreator *reducer = new TreeCreator();
@@ -47,6 +70,7 @@ void skim_ntuples_with_selection() {
     for (auto filename : filenames) {
         reducer->read(oldpath + filename, treename);
 
+        reducer->setBranchesToCopy(branches_to_copy);
         reducer->setCut(selection);
 
         reducer->write(newpath + filename, treename);
