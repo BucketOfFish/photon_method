abb53d41f7c245327974ef7987dbcaad3c31f022
Matt Zhang <set.stun@gmail.com>
2020-07-13 20:27:33 +0200

    Some trigger table stuff that was never committed

--- Misc/Tools/SUSYTriggers_Table.cpp
+++ Misc/Tools/SUSYTriggers_Table.cpp
@@ -7,7 +7,7 @@ void SUSYTriggers_Table() {
     string ntuple_path = "/eos/atlas/atlascerngroupdisk/phys-susy/2L2J-ANA-SUSY-2018-05/SusySkim2LJets/v1.7/SUSY2/";
     vector<string> file_names {"SUSY2_Data/data15-16", "SUSY2_Data/data17", "SUSY2_Data/data18", "SUSY2_Signal_mc16a/C1N2_WZ_600_0_2L2J", "SUSY2_Signal_mc16cd/C1N2_WZ_600_0_2L2J", "SUSY2_Signal_mc16e/C1N2_WZ_600_0_2L2J", "SUSY2_Signal_mc16a/C1N2_WZ_200p0_100p0_2L2J", "SUSY2_Signal_mc16cd/C1N2_WZ_200p0_100p0_2L2J", "SUSY2_Signal_mc16e/C1N2_WZ_200p0_100p0_2L2J"};
     vector<string> file_labels {"data15-16", "data17", "data18", "(600, 0) MC16a", "(600, 0) MC16cd", "(600, 0) MC16e", "(200, 100) MC16a", "(200, 100) MC16cd", "(200, 100) MC16e"};
-    vector<string> regions = {"Inclusive", "Baseline", "VRLow", "VRMed", "VRHigh"};
+    vector<string> regions = {"trig_eff_baseline"};
     vector<string> triggers {"1", "trigMatch_1L2LTrig", "trigMatch_1L2LTrigOR", "trigMatch_2LTrig", "trigMatch_2LTrigOR"};
     vector<string> flavors {"lepFlavor[0]==1 && lepFlavor[1]==1", "lepFlavor[0]==2 && lepFlavor[1]==2", "((lepFlavor[0]==2 && lepFlavor[1]==1) || (lepFlavor[0]==1 && lepFlavor[1]==2))"};
 
@@ -25,14 +25,14 @@ void SUSYTriggers_Table() {
 
         vector<vector<vector<int>>> file_yields;
         for (string region : regions) {
-            cout << "\t" << region << endl;
+            cout << "\t" << NMinus1Cut(cuts::selections[region], "trigMatch_2LTrigOR") << endl;
             vector<vector<int>> region_yields;
             for (string trigger : triggers) {
                 cout << "\t\t" << trigger << endl;
                 vector<int> trigger_yields;
                 for (string flavor : flavors) {
                     trigger_yields.push_back(tch->GetEntries(
-                        cuts::selections[region] + TCut(trigger.c_str()) + TCut(flavor.c_str())
+                        NMinus1Cut(cuts::selections[region], "trigMatch_2LTrigOR") + TCut(trigger.c_str()) + TCut(flavor.c_str())
                     ));
                 }
                 region_yields.push_back(trigger_yields);
@@ -60,8 +60,8 @@ void SUSYTriggers_Table() {
             boost::replace_all(trigger, "_", "\\_");
             boost::replace_all(trigger, "&", "\\&");
             fout << "\\begin{center}" << endl;
-            if (t == 0) fout << "Inclusive Counts in " << region_name << " Region \\\\" << endl;
-            else fout << trigger << " Counts in " << region_name << " Region \\\\" << endl;
+            if (t == 0) fout << "Total Events in " << region_name << " Region \\\\" << endl;
+            else fout << trigger << " Events in " << region_name << " Region \\\\" << endl;
             fout << "\\begin{tabular}{l|l|l|l}" << endl;
             fout << "& ee & mm & em \\\\" << endl;
             fout << "\\hline" << endl;
@@ -78,7 +78,7 @@ void SUSYTriggers_Table() {
 
     //--- efficiency tables
     cout << setprecision(2) << fixed;
-    for (int r=1; r<regions.size(); r++) {
+    for (int r=0; r<regions.size(); r++) {
         string region_name = regions[r];
         for (int t=1; t<triggers.size(); t++) {
             string trigger = triggers[t];
